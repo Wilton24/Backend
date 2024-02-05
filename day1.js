f8bb532493014b1a280067f167be13df98204795
Wilton24 <cbasher24@gmail.com>
2024-02-05 14:45:11 +0800

    First commit, done with the native module, off the the Node Package Manager

---
+++ day1.js
@@ -0,0 +1,11 @@
+const { log } = require("console");
+const fs = require("fs");
+
+// fs.writeFile("text.txt", "YellowSpak", (err) => {
+//   if (err) throw err;
+// });
+
+fs.readFile("newpath/dagonMiester.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
